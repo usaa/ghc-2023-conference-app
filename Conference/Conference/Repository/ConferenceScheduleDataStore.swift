348ad7dc696666d6b843b06cd19476813c2cf925
Pham, Ashley (KZ82975) <ashley.pham@usaa.com>
2023-08-28 15:37:09 -0500

    Caching

--- Conference/Conference/Repository/ConferenceScheduleDataStore.swift
+++ Conference/Conference/Repository/ConferenceScheduleDataStore.swift
@@ -13,15 +13,72 @@ public class ConferenceScheduleDataStore {
 
     @Published private(set) var currentValue: ConferenceScheduleResult = ConferenceScheduleResult.uninitialized
     lazy var data: AnyPublisher<ConferenceScheduleResult, Never> = $currentValue.eraseToAnyPublisher()
+    var minutesToLive: Double
+    private var secondsToLive: TimeInterval {
+        return minutesToLive * 60
+    }
+    var expired: Bool { return dataAge + secondsToLive < currentTime() }
 
-    init() {
+    var dateCached: Date? {
+        if dataAge == 0 {
+            return nil
+        } else {
+            return Date(timeIntervalSince1970: dataAge)
+        }
+    }
+    var dataAge: TimeInterval = 0
+    /// Returns the current time in seconds, used to determine lifespan of data
+    var currentTime: () -> TimeInterval = { Date().timeIntervalSince1970 }
+
+    public init(minutesToLive: Double = 5.0) {
+        self.minutesToLive = minutesToLive
+        if let storedTime = UserDefaults.standard.data(forKey: "ConferenceScheduleStorageTime"),
+           let storageTime = try? JSONDecoder().decode(StorageTime.self, from: storedTime) {
+            dataAge = storageTime.storageTimeInterval
+            if let storedData = UserDefaults.standard.data(forKey: "ConferenceSchedule"),
+               let cache = try? JSONDecoder().decode(ConferenceSchedule.self, from: storedData) {
+                if expired {
+                    clear()
+                } else {
+                    put(.success(data:cache))
+                }
+            }
+        }
     }
 
     func put(_ result: ConferenceScheduleResult) {
+        switch(result) {
+        case .uninitialized:
+            dataAge = 0
+        default:
+            dataAge = currentTime()
+        }
+        self.persist(schedule: result)
         currentValue = result
     }
 
     func clear() {
+        dataAge = 0
         currentValue = ConferenceScheduleResult.uninitialized
+    }
+    
+    private func persist(schedule: ConferenceScheduleResult) {
+        //persist data
+        let data = schedule.data
+        guard let encodedData = try? JSONEncoder().encode(data) else { return }
+        UserDefaults.standard.set(encodedData, forKey: "ConferenceSchedule")
+        //persist time of data
+        guard let encodedStorageTime = try? JSONEncoder().encode(StorageTime(timeInterval: currentTime())) else { return }
+        UserDefaults.standard.set(encodedStorageTime, forKey: "ConferenceScheduleStorageTime")
+    }
+}
+
+class StorageTime: Codable {
+    private var storageTime: Double
+    var storageTimeInterval: TimeInterval {
+        return storageTime / 1000
+    }
+    init(timeInterval: TimeInterval) {
+        self.storageTime = timeInterval * 1000
     }
 }
