e68f80e094c95657d3f253deddf70b1f51d9576e
Pham, Ashley (kz82975) <ashley.pham@usaa.com>
2023-08-28 15:47:36 -0500

    Create initial conference schedule view

---
+++ Conference/Conference/ConferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConferenceApp.swift
+//  Conference
+//
+//  Created by Pham, Ashley on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ConferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conference/Conference/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Conference
+//
+//  Created by Pham, Ashley on 8/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ConferenceScheduleView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
