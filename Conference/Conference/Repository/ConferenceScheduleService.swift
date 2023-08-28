0ba72ca6ebfc96408ac323fa9e0abbdd5cb02d11
Pham, Ashley (kz82975) <ashley.pham@usaa.com>
2023-08-28 15:47:36 -0500

    Expand on the mocked service

--- Conference/Conference/Repository/ConferenceScheduleService.swift
+++ Conference/Conference/Repository/ConferenceScheduleService.swift
@@ -61,21 +61,4 @@ class ConferenceScheduleService {
         return self.conferenceSchedule
     }
     
-    func register(session: Session) async -> ConferenceSchedule {
-        if let s = self.conferenceSchedule.sessions?.firstIndex(of: session) {
-            var registerSession = session
-            registerSession.isRegistered = true
-            self.conferenceSchedule.sessions?[s] = registerSession
-        }
-        return self.conferenceSchedule
-    }
-
-    func unregister(session: Session) async -> ConferenceSchedule {
-        if let s = self.conferenceSchedule.sessions?.firstIndex(of: session) {
-            var registerSession = session
-            registerSession.isRegistered = false
-            self.conferenceSchedule.sessions?[s] = registerSession
-        }
-        return self.conferenceSchedule
-    }
 }
